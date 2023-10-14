9754aba5272e11772395018a71c0e8a2752847df
Devansh Sharma <devansh.sharma@gmail.com>
2023-10-14 17:56:00 +0530

    Update packages; Update typescript-eslint config for v6; Update prettier config for v3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,8 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
